fb6b8d5cdb32f72562a739913308ebeffe32aeff
Asus <capital.phyoethurakyaw@gmail.com>
2021-09-11 05:41:50 +0630

    Add project files.

---
+++ st/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace st
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String ptk = null;
+            Console.WriteLine(ptk.ToString());
+            Console.ReadLine();
+        }
+    }
+}
